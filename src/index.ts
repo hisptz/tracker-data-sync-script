752aaf265fd402bb5e1bb3bbe8a3c6ad3a720c25
Gift <nnkogift@hotmail.com>
2022-05-18 19:20:30 +0300

    :sparkles: feat(commander): Implementation
    - Implemented commander cli framework

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {config} from "dotenv";
 import FilesService from "./utils/files";
 import {DataUploadService} from "./data-upload";
 import SummaryService from "./summary";
+import {Command} from "commander";
 
 
 export default class DataSync {
@@ -37,5 +38,31 @@ export default class DataSync {
 
 }
 
+
+const program = new Command();
+
+program.name("tracker-data-sync")
+    .description("Sync tracker data between 2 DHIS2 instances")
+    .version("1.0.0");
+
+
+program.command("sync")
+    .option("-d --duration <duration>", "Duration of the extraction in days", "30")
+    .option("-p --page-size <page-size>", "Page size of the extraction", "100")
+    .option("-u --upload-concurrency <upload-concurrency>", "Concurrency of the upload", "1")
+    .option("-d --download-concurrency <download-concurrency>", "Concurrency of the download", "1")
+    .action(async (arg) => {
+
+        const dataSync = new DataSync(Number(arg.duration), Number(arg.pageSize), {
+            download: Number(arg.downloadConcurrency),
+            upload: Number(arg.uploadConcurrency),
+
+        });
+
+        console.info(`Starting data sync: Duration: ${arg.duration}, Page size: ${arg.pageSize}, Upload concurrency: ${arg.uploadConcurrency}, Download concurrency: ${arg.downloadConcurrency}`);
+        console.info("Warning: This will delete previously generated files");
+        await dataSync.sync(true);
+    });
+
 config();
-new DataSync(200, 50).sync(true);
+program.parse(process.argv);
